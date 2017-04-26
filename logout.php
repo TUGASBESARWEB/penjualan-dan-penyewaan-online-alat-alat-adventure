f68cd6ffc08060b5520bb4483b4fce55cb5d9dae
AldiPrasetya <aldidaniprasetya@gmail.com>
2017-04-27 00:34:08 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['user_admin'],$_SESSION['nama_admin'],$_SESSION['pass_admin'],$_SESSION['sesidunit'],$_SESSION['sesvalid']);
-echo "<script>alert('Anda telah keluar dari halaman administrator'); window.location = '../'</script>";
-?>
