07e85aefd970bcae77db59868474df44c3867f22
AldiPrasetya <aldidaniprasetya@gmail.com>
2017-04-27 00:29:10 +0700

    Delete editorder.php

--- editorder.php
+++
@@ -1,6 +0,0 @@
-<?php
-mysqli_query($con,"delete from pekerjaan where id_order='$_GET[id]'");
-mysqli_query($con,"delete from biaya where id_order='$_GET[id]'");
-mysqli_query($con,"delete from orders where id_order='$_GET[id]'");
-echo "<meta http-equiv=refresh content='0;url=?module=order'>";
-?>
