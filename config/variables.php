13e3eb271c4e223f7383fb83502be72d6479ccf0
khavidAshari <khafidazh@gmail.com>
2017-04-27 01:19:04 +0700

    Delete variables.php

--- config/variables.php
+++
@@ -1,8 +0,0 @@
-<?php
-$arbulan=array("","Januari","Pebruari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","Desember");
-$arjk=array("Laki-laki","Perempuan");
-$argoldar=array("A","B","O","AB");
-$arkawin=array("Kawin","Tidak Kawin");
-$arbpjs=array("Non BPJS","BPJS");
-$artipebpjs=array("Non Prolanis","Prolanis");
-?>
